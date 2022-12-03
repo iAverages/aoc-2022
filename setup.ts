36249396203bb3f2bbd159f4c67f32e37254bfc5
iAverages <31937542+iAverages@users.noreply.github.com>
2022-12-03 13:02:37 +0000

    remove +1 again lol

--- setup.ts
+++ setup.ts
@@ -3,7 +3,7 @@ const DEFAULT_CONTENT = `export {};
 const input = await Deno.readTextFile(new URL("./input.txt", import.meta.url));
 `;
 
-const day = new Date().getDate() + 1;
+const day = new Date().getDate();
 
 console.log(`Day ${day}, only ${25 - day} days left.`);
 
