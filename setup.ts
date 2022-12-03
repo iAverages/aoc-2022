0acd21df38096f174ff5ed91be458ce774e4e59b
iAverages <31937542+iAverages@users.noreply.github.com>
2022-12-03 14:07:16 +0000

    Dont need export {};

--- setup.ts
+++ setup.ts
@@ -1,7 +1,4 @@
-const DEFAULT_CONTENT = `export {};
-
-const input = await Deno.readTextFile(new URL("./input.txt", import.meta.url));
-`;
+const DEFAULT_CONTENT = `const input = await Deno.readTextFile(new URL("./input.txt", import.meta.url));`;
 
 const day = new Date().getDate();
 
